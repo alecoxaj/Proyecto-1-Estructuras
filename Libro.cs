8b87ab991c3eba42f4040d8010c2b94b95d3d14b
Alejandro <alejandrocoxaj94@gmail.com>
2026-09-07 19:57:41 -0600

    Alejandro agregó los datos de muestra del libro

--- Libro.cs
+++ Libro.cs
@@ -33,4 +33,16 @@ namespace CatalogoBiblioteca
             // Un libro nuevo inicia sin préstamos
             VecesPrestado = 0;
         }
-}   }
+
+        // Muestra los datos de un libro
+        public void Mostrar()
+        {
+            Console.WriteLine("Código: " + Codigo);
+            Console.WriteLine("Título: " + Titulo);
+            Console.WriteLine("Autor: " + Autor);
+            Console.WriteLine("Categoría: " + Categoria);
+            Console.WriteLine("Copias disponibles: " + CopiasDisponibles);
+            Console.WriteLine("Veces prestado: " + VecesPrestado);
+       }
+    }
+}
